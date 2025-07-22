8a6ab3758aa9c32b88cb5ea634ff38db4230c536
abdourahamanetinkindjeeri99 <abdourahamanetinkindjeeri99@gmail.com>
2025-07-22 23:49:41 +0000

    Correction des erreurs avant le deployement

--- app/config/env.php
+++ app/config/env.php
@@ -4,8 +4,13 @@ namespace App\Config;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
-$dotenv->load();
+// $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+// $dotenv->load();
+
+if (file_exists(__DIR__ . '/../../.env')) {
+  \Dotenv\Dotenv::createImmutable(__DIR__ . '/../../')->load();
+}
+
 
 define('DSN', $_ENV['DSN'] ?? 'mysql:host=localhost;dbname=maxitsa');
 define('USER', $_ENV['DB_USER'] ?? 'root');
