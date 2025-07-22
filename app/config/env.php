06c9169acc9c33543d625046d7da3acaf8fc85d7
abdourahamanetinkindjeeri99 <abdourahamanetinkindjeeri99@gmail.com>
2025-07-22 23:56:37 +0000

    Correction des erreurs avant le deployement

--- app/config/env.php
+++ app/config/env.php
@@ -15,11 +15,11 @@ if (file_exists(__DIR__ . '/../../.env')) {
 define('DSN', $_ENV['DSN'] ?? 'mysql:host=localhost;dbname=maxitsa');
 define('USER', $_ENV['DB_USER'] ?? 'root');
 define('PASSWORD', $_ENV['DB_PASSWORD'] ?? '');
-define('TWILIO_SID', $_ENV['TWILIO_SID'] ?? '');
-define('TOKEN', $_ENV['TOKEN'] ?? '');
-define('PHONE', $_ENV['PHONE'] ?? '');
+// define('TWILIO_SID', $_ENV['TWILIO_SID'] ?? '');
+// define('TOKEN', $_ENV['TOKEN'] ?? '');
+// define('PHONE', $_ENV['PHONE'] ?? '');
 define('BASE_URL', $_ENV['BASE_URL'] ?? 'http://localhost:8080/');
-define('MESSAGING_SID', $_ENV['MESSAGING_SID']);
+// define('MESSAGING_SID', $_ENV['MESSAGING_SID']);
 define('PRIVATE_KEY', $_ENV['PRIVATE_KEY']);
 define('PUBLIC_KEY', $_ENV['PUBLIC_KEY']);
 define('CLOUD_NAME', $_ENV['CLOUD_NAME']);
